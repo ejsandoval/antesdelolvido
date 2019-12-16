430249cc834973d42d1cfe81fdf86a9527eeccfa
Esteban <ejsandoval@uc.cl>
2019-12-16 09:41:50 -0300

    Updates

---
+++ precache-manifest.de726a7e3430cb6ed304871b1f383ee7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309b9d9b0d478fdcb5ab6220ebc0d771",
+    "url": "/antesdelolvido/index.html"
+  },
+  {
+    "revision": "3337c7ce0a99c20cdad8",
+    "url": "/antesdelolvido/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "55135e6fec19814009ae",
+    "url": "/antesdelolvido/static/js/2.51cf159d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/antesdelolvido/static/js/2.51cf159d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3337c7ce0a99c20cdad8",
+    "url": "/antesdelolvido/static/js/main.58cb14f2.chunk.js"
+  },
+  {
+    "revision": "f6bc6b894f47635464cf",
+    "url": "/antesdelolvido/static/js/runtime-main.88828408.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/antesdelolvido/static/media/logo.5d5d9eef.svg"
+  }
+]);
